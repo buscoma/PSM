a48a25bf1d9459cf02b1d138b7a21ef6ead8c63f
buscoma <matias.busco@mercadolibre.com>
2020-05-11 17:40:03 -0300

    estandarización grafos

--- PSM/src/misApis/GrafoTDA.java
+++ PSM/src/misApis/GrafoTDA.java
@@ -3,19 +3,19 @@ package misApis;
 import misApis.ConjuntoTDA;
 
 public interface GrafoTDA {
-	  void inicializarGrafo();
-	  
-	  void agregarVertice(int paramInt);
-	  
-	  void eliminarVertice(int paramInt);
-	  
-	  ConjuntoTDA vertices();
-	  
-	  void agregarArista(int paramInt1, int paramInt2, int paramInt3);
-	  
-	  void eliminarArista(int paramInt1, int paramInt2);
-	  
-	  boolean existeArista(int paramInt1, int paramInt2);
-	  
-	  int pesoArista(int paramInt1, int paramInt2);
-	}
+	void inicializarGrafo();
+
+	void agregarVertice(int paramInt);
+
+	void eliminarVertice(int paramInt);
+
+	ConjuntoTDA vertices();
+
+	void agregarArista(int paramInt1, int paramInt2, int paramInt3);
+
+	void eliminarArista(int paramInt1, int paramInt2);
+
+	boolean existeArista(int paramInt1, int paramInt2);
+
+	int pesoArista(int paramInt1, int paramInt2);
+}
